e7cadce9fb2807eee7fe364912205c50f393331f
Red-Nut <james.barrenger@gmail.com>
2022-08-25 20:48:37 +1000

    add url include paths

--- agf_assets/urls.py
+++ agf_assets/urls.py
@@ -14,14 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    # Documents
-    path('documents/', include('agf_documents.urls'))
-
-    # Assets
-    path('assets/', include('agf_assets.urls'))
+    
 ]
