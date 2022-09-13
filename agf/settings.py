6485de2d4b88a4f66d6335e3a0414f4cee944514
Red-Nut <james.barrenger@gmail.com>
2022-09-13 17:06:06 +1000

    trying same origin again

--- agf/settings.py
+++ agf/settings.py
@@ -79,7 +79,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'agf.wsgi.application'
 
-X_FRAME_OPTIONS = 'ALLOWALL'
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
