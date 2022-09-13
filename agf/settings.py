61d2fd4292f1807c61a57d8a291ac5d3d664b3e3
Red-Nut <james.barrenger@gmail.com>
2022-09-13 17:04:14 +1000

    xframe allow all

--- agf/settings.py
+++ agf/settings.py
@@ -79,7 +79,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'agf.wsgi.application'
 
-X_FRAME_OPTIONS = 'SAMEORIGIN'
+X_FRAME_OPTIONS = 'ALLOWALL'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
