2a46bd643194d159c5652833a35d4c4b3a3ec954
Red-Nut <james.barrenger@gmail.com>
2022-12-01 22:25:52 +1000

    propoer link to index.html

--- agf/views.py
+++ agf/views.py
@@ -4,4 +4,4 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def Index(request):
 
-    return render(request, "index.html")
+    return render(request, "agf/index.html")
